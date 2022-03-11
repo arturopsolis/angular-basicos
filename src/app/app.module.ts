f34a7bd5bb557aee360ac0d68c910d3ba01e5670
Arturo Pacheco <arturo@MacBook-Pro-de-Arturo.local>
2022-03-10 21:13:19 -0600

    Creacion de modulos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { ContadorModule } from './contador/contador.module';
+
+import { HeoresModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeoresModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -10,7 +10,7 @@ export class ListadoComponent{
   heroes:string[] = ['Ironman', 'Spiderman', 'Hulk', 'Capitan America', 'Thor'];
   heroeBorrado:string = '';
   borrarHeroe(){
-    this.heroeBorrado = this.heroes.shift() || '';
+    this.heroeBorrado = this .heroes.shift() || '';
   }
 
 }
